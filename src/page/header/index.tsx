4ea84d64609b9809977f043aba1227972ab84cc4
pang <1973503139@qq.com>
2024-01-27 19:46:13 +0800

    --style 结构修改

--- src/page/header/index.tsx
+++ src/page/header/index.tsx
@@ -5,53 +5,47 @@ export default function Header() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <header className="header">
-        <nav className="nav nav--tours">
-          <a
-            href="##"
-            className="nav__el"
-            onClick={(event) => {
-              event.preventDefault();
-              navigate("/");
-            }}
-          >
-            All tours
-          </a>
-          <form className="nav__search">
-            <button className="nav__search-btn">
-              <svg>
-                <use xlinkHref="img/icons.svg#icon-search"></use>
-              </svg>
-            </button>
-            <input
-              type="text"
-              placeholder="Search tours"
-              className="nav__search-input"
-            />
-          </form>
-        </nav>
-        <div className="header__logo">
-          <img src="/img/logo-white.png" alt="Natours logo" />
-        </div>
-        <nav className="nav nav--user">
-          <a href="###" className="nav__el">
-            My bookings
-          </a>
-          <a href="###" className="nav__el">
-            <img
-              src="/img/user.jpg"
-              alt="UserPhoto"
-              className="nav__user-img"
-            />
-            <span>Jonas</span>
-          </a>
-          {/* 这里需要做个判断，通过接口判断是否登录 */}
-          {/* 
-          <button className="nav__el">Log in</button>
-          <button className="nav__el nav__el--cta">Sign up</button> */}
-        </nav>
-      </header>
-    </div>
+    <header className="header">
+      <nav className="nav nav--tours">
+        <a
+          href="##"
+          className="nav__el"
+          onClick={(event) => {
+            event.preventDefault();
+            navigate("/");
+          }}
+        >
+          All tours
+        </a>
+        <form className="nav__search">
+          <button className="nav__search-btn">
+            <svg>
+              <use xlinkHref="img/icons.svg#icon-search"></use>
+            </svg>
+          </button>
+          <input
+            type="text"
+            placeholder="Search tours"
+            className="nav__search-input"
+          />
+        </form>
+      </nav>
+      <div className="header__logo">
+        <img src="/img/logo-white.png" alt="Natours logo" />
+      </div>
+      <nav className="nav nav--user">
+        <a href="###" className="nav__el">
+          My bookings
+        </a>
+        <a href="###" className="nav__el">
+          <img src="/img/user.jpg" alt="UserPhoto" className="nav__user-img" />
+          <span>Jonas</span>
+        </a>
+        {/* 这里需要做个判断，通过接口判断是否登录 */}
+        {/* 
+      <button className="nav__el">Log in</button>
+      <button className="nav__el nav__el--cta">Sign up</button> */}
+      </nav>
+    </header>
   );
 }
