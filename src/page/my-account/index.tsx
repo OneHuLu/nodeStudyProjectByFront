66074102837584c8586fead9b7187a6350335dab
pang <1973503139@qq.com>
2024-02-20 20:51:54 +0800

    --style 删除未使用变量函数

--- src/page/my-account/index.tsx
+++ src/page/my-account/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Upload, UploadFile } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
