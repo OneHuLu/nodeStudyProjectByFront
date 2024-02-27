57825d5ce952545cf560fa2115a9b15e663da310
pang <1973503139@qq.com>
2024-02-27 18:20:30 +0800

    --style 文件结构修改

--- src/page/content/index.tsx
+++ src/page/content/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getAllTours } from "./content";
-import ContentSkeleton from "page/skeleton/content-skeleton";
+import ContentSkeleton from "page/skeleton/content-skeleton/content-skeleton";
 import Tour from "./component/tour";
 import { useDispatch, useSelector } from "react-redux";
 
